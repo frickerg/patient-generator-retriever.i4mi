d87f04c774d4a44e6aeae62f396da3056c7365b2
Guillaume Fricker <fricker.g@zoho.com>
2019-04-16 11:47:35 +0200

    create SQLite database file

--- retriever.js
+++ retriever.js
@@ -1,10 +1,31 @@
 const request = require('request');
+const path = require('path');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(generateFileName('database'));
+
+const uri = 'http://patient-generator.i4mi.bfh.ch/patient/get';
 const config = {
 	json: true
 };
-const uri = 'http://patient-generator.i4mi.bfh.ch/patient/get';
 
 request(uri, config, (err, res, body) => {
+	db.serialize(function () {
+		db.run("CREATE TABLE lorem (info TEXT)");
+
+		var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+		for (var i = 0; i < 10; i++) {
+			stmt.run("Ipsum " + i);
+		}
+		stmt.finalize();
+
+		db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+			console.log(row.id + ": " + row.info);
+		});
+	});
+
+	db.close();
+
 	if (err) {
 		return console.log(err);
 	}
@@ -15,3 +36,16 @@ request(uri, config, (err, res, body) => {
 	}
 	console.log(body);
 });
+
+function generateFileName(directory) {
+	let extension = 'sqlite3';
+
+	let timestamp = new Date().toISOString().substring(0, 10);
+	timestamp += '-';
+	timestamp += Date.now();
+
+	console.log(timestamp);
+	let filename = 'patdata-' + timestamp + '.' + extension;
+
+	return path.join(__dirname, directory, filename);
+}

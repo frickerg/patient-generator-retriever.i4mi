eb1ccee243d9831a9a9a1826d1412459b5005d9c
Guillaume Fricker <fricker.g@zoho.com>
2019-04-16 19:24:45 +0200

    Testing with two APIs

--- retriever.js
+++ retriever.js
@@ -4,38 +4,45 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(generateFileName('database'));
 
-const uri = 'http://patient-generator.i4mi.bfh.ch/patient/get';
+const uri_bfh = 'http://patient-generator.i4mi.bfh.ch/patient/get';
+const uri_mockaroo = 'https://my.api.mockaroo.com/lep_demonstrator_patient_mock.json?key=e7b8fb30';
 const config = {
 	json: true
 };
 
-request(uri, config, (err, res, body) => {
-	db.serialize(function () {
-		db.run("CREATE TABLE lorem (info TEXT)");
+run();
 
-		var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-		for (var i = 0; i < 10; i++) {
-			stmt.run("Ipsum " + i);
-		}
-		stmt.finalize();
-
-		db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-			console.log(row.id + ": " + row.info);
-		});
+async function run() {
+	let promiseBFH = new Promise((resolve, reject) => {
+		return resolve(retrieveBFHData());
 	});
 
-	db.close();
+	let promiseMockaroo = new Promise((resolve, reject) => {
+		return resolve(retrieveMockarooData());
+	});
 
-	if (err) {
-		return console.log(err);
-	}
+	writeToDatabase(await promiseBFH, await promiseMockaroo);
+}
 
-	let birthDate = new Date(body.birthDate);
-	if (birthDate.getFullYear() > 2000) {
-		return;
-	}
-	console.log(body);
-});
+function retrieveBFHData() {
+	return request(uri_bfh, config, (err, res, body) => {
+		if (err) {
+			return console.log(err);
+		}
+
+		let birthDate = new Date(body.birthDate);
+		if (birthDate.getFullYear() > 2000) {
+			return JSON.stringify({});
+		}
+		return body;
+	});
+}
+
+function retrieveMockarooData() {
+	return request(uri_mockaroo, config, (err, res, body) => {
+		return body;
+	});
+}
 
 function generateFileName(directory) {
 	let extension = 'sqlite3';
@@ -48,4 +55,57 @@ function generateFileName(directory) {
 	let filename = 'patdata-' + timestamp + '.' + extension;
 
 	return path.join(__dirname, directory, filename);
+}
+
+function writeToDatabase(bfhData, mockarooData) {
+	const GAP = ', ';
+	let last_name = bfhData.name.family;
+	let first_name = bfhData.name.given.join(' ');
+	let gender = bfhData.gender;
+	let birthDate = bfhData.birthDate.toISOString();
+
+	let street = bfhData.address.line[0];
+	let city = bfhData.address.city;
+	let state = bfhData.address.state;
+	let postalCode = bfhData.address.postalCode;
+
+	let treatment_type = mockarooData.treatment_type;
+	let insurance_type = mockarooData.insurance_type;
+	let pid = mockarooData.pid;
+	let social_security_number = mockarooData.social_security_number;
+	let date_of_entry = mockarooData.date_of_entry;
+	let date_of_departure = mockarooData.date_of_departure;
+
+	db.serialize(function () {
+		db.run("CREATE TABLE Patient (UID, PID, SSN, FirstName, LastName, Gender, BirthDate, TreatmentType, InsuranceType, AddrStreet, AddrZip, AddrCity, DateOfEntry, DateOfDeparture)");
+
+		db.run('INSERT INTO Patient VALUES (' +
+			uid + GAP +
+			pid + GAP +
+			social_security_number + GAP +
+			first_name + GAP +
+			last_name + GAP +
+			gender + GAP +
+			birthDate + GAP +
+			treatment_type + GAP +
+			insurance_type + GAP +
+			street + GAP +
+			postalCode + GAP +
+			city + GAP +
+			date_of_entry + GAP +
+			date_of_departure
+		);
+
+	});
+
+	db.close();
+}
+
+function isEmpty(obj) {
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop))
+			return false;
+	}
+
+	return JSON.stringify(obj) === JSON.stringify({});
 }

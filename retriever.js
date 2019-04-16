aa622fa3458f02ad1caa2467f5547a8491ea3af2
Guillaume Fricker <fricker.g@zoho.com>
2019-04-16 11:14:53 +0200

    .editorconfig added

--- retriever.js
+++ retriever.js
@@ -1,24 +1,17 @@
 const request = require('request');
 const config = {
-    json: true
+	json: true
 };
+const uri = 'http://patient-generator.i4mi.bfh.ch/patient/get';
 
-request('http://patient-generator.i4mi.bfh.ch/patient/get', config, (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(body);
+request(uri, config, (err, res, body) => {
+	if (err) {
+		return console.log(err);
+	}
 
-    let birthDate = new Date(body.birthDate);
-    if (birthDate.getFullYear() > 2000) {
-        return;
-    }
-    console.log(body);
-
-    knex.schema.createTable('users', function (table) {
-        table.increments();
-        table.string('name');
-        table.timestamps();
-    });
-
+	let birthDate = new Date(body.birthDate);
+	if (birthDate.getFullYear() > 2000) {
+		return;
+	}
+	console.log(body);
 });

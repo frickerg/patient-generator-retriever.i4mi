3605d0e5a1273b0c0b754676bf7130773c49dd4b
Guillaume Fricker <fricker.g@zoho.com>
2019-04-17 22:01:42 +0200

    Query building works like a charm 😎

--- retriever.js
+++ retriever.js
@@ -6,6 +6,11 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(generateFileName('database'));
 
+const requested_lenght = 100;
+const GAP = ', ';
+const QUOTE = '\'';
+const BREAK = ');';
+
 const config_bfh = {
 	uri: 'http://patient-generator.i4mi.bfh.ch/patient/get',
 	headers: {
@@ -22,31 +27,36 @@ const config_mockaroo = {
 	json: true
 };
 
-run();
+run().then((result) => {
+	if (result[0].length === requested_lenght && result[1].length === requested_lenght) {
+		writeToDatabase(result[0], result[1]);
+	}
+});
 
 async function run() {
+	// Retrieve BFH Data
 	let bfhArray = [];
-
-	while (bfhArray.length < 100) {
+	while (bfhArray.length < requested_lenght) {
 		let promiseBFH = new Promise((resolve, reject) => {
 			return resolve(retrieveBFHData());
 		});
 
 		let resolved = await promiseBFH;
-		if (!isEmpty(resolved)) {
+		if (resolved != JSON.stringify({})) {
+			console.log(resolved);
 			bfhArray.push(resolved);
 		}
 	}
 	console.log(bfhArray.length);
 
-	/*
+	// Retrieve Mockaroo Data
 	let promiseMockaroo = new Promise((resolve, reject) => {
-		return retrieveMockarooData();
+		return resolve(retrieveMockarooData());
 	});
-	let leresult = await promiseMockaroo;
-	*/
+	let mockarooArray = await promiseMockaroo;
+	console.log(mockarooArray.length);
 
-	//writeToDatabase(await promiseBFH, await promiseMockaroo);
+	return [bfhArray, mockarooArray];
 }
 
 function retrieveBFHData() {
@@ -87,52 +97,51 @@ function generateFileName(directory) {
 }
 
 function writeToDatabase(bfhData, mockarooData) {
-	const GAP = ', ';
-	console.log(bfhData.name[0].family)
-	let last_name = bfhData.name[0].family;
-	let first_name = bfhData.name[0].given.join(' ');
-	let gender = bfhData.gender;
-	let birthDate = new Date(bfhData.birthDate).toISOString();
-
-	let street = bfhData.address[0].line[0];
-	let city = bfhData.address[0].city;
-	let postalCode = bfhData.address[0].postalCode;
-
-	let treatment_type = mockarooData.treatment_type;
-	let insurance_type = mockarooData.insurance_type;
-	let pid = mockarooData.pid;
-	let social_security_number = mockarooData.social_security_number;
-	let date_of_entry = mockarooData.date_of_entry;
-	let date_of_departure = mockarooData.date_of_departure;
-
 	db.serialize(function () {
 		db.run("CREATE TABLE Patient (PID, SSN, FirstName, LastName, Gender, BirthDate, TreatmentType, InsuranceType, AddrStreet, AddrZip, AddrCity, DateOfEntry, DateOfDeparture)");
+		for (let i = 0; i < requested_lenght; i++) {
+			let bfhItem = bfhData[i];
+			let mockarooItem = mockarooData[i]
 
-		db.run('INSERT INTO Patient VALUES (' +
-			pid + GAP +
-			social_security_number + GAP +
-			first_name + GAP +
-			last_name + GAP +
-			gender + GAP +
-			birthDate + GAP +
-			treatment_type + GAP +
-			insurance_type + GAP +
-			street + GAP +
-			postalCode + GAP +
-			city + GAP +
-			date_of_entry + GAP +
-			date_of_departure
-		);
-
+			let query = 'INSERT INTO Patient VALUES (' +
+				createQueryString(mockarooItem.pid) +
+				createQueryString(mockarooItem.social_security_number) +
+				createQueryString(bfhItem.name[0].given[0]) +
+				createQueryString(bfhItem.name[0].family) +
+				createQueryString(bfhItem.gender) +
+				createQueryString(new Date(bfhItem.birthDate).toISOString()) +
+				createQueryString(mockarooItem.treatment_type) +
+				createQueryString(mockarooItem.insurance_type) +
+				createQueryString(bfhItem.address[0].line[0]) +
+				createQueryString(bfhItem.address[0].postalCode) +
+				createQueryString(bfhItem.address[0].city) +
+				createQueryString(mockarooItem.date_of_entry) +
+				createQueryString(mockarooItem.date_of_departure, BREAK);
+			console.log(query);
+			console.log();
+			db.run(query);
+		}
 	});
 
 	db.close();
 }
 
-function isEmpty(obj) {
-	for (var prop in obj) {
-		if (obj.hasOwnProperty(prop))
-			return false;
+function createQueryString(line, param) {
+	// optimize and watch out for the gap and param check
+	let queryPartition;
+
+	switch (typeof line) {
+		case 'string':
+			queryPartition = QUOTE + line + QUOTE;
+			break;
+		case 'number':
+			queryPartition = line;
+		default:
+			queryPartition = null;
+			break;
 	}
-	return JSON.stringify(obj) === JSON.stringify({});
+	if (param) {
+		return queryPartition + param;
+	}
+	return queryPartition + GAP;
 }
